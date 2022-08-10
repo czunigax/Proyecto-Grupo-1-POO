a0559bcfbc31181f470938645611cc9a1fa31972
czunigac99 <czunigac@unah.hn>
2022-08-10 17:51:24 -0500

    Creando la clase clientecontroller y el metodo para insertar en tablas

---
+++ EjemploCRUD1/src/main/java/controller/ClienteController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import modelo.Persona;
+import view.PersonaView;
+import unah.ejemplocrud1.ConectarBD;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cris
+ */
+public class ClienteController {
+    private Cliente Modelo;
+    private ClienteView Vista;
+    
+    private boolean exito;
+    private Statement st;
+    private Connection cn;
+    private ResultSet rs;
+    private String sql;
+    
+        public ClienteController(Cliente Modelo, ClienteView Vista){
+        this.Modelo= Modelo;
+        this.Vista= Vista;
+        }
+        
+         public String getId() {
+        return Modelo.getId();
+    }
+
+    public void setId(String Id) {
+        this.Modelo.setId(Id);
+    }
+
+    public String getNombre() {
+    return this.Modelo.getNombre();
+    }
+
+    public void setNombre(String Nombre) {
+        this.Modelo.setNombre(Nombre);
+    } 
+    
+    public String getDireccion(){
+    return this.Modelo.getDireccion();
+    }
+    public void setDireccion(String Direccion){
+    this.Modelo.setDireccion();
+    }
+    
+    public void ActualizarVistaCliente(){
+        Vista.ImprimirDatospersona(Modelo.getId(), Modelo.getNombre(), Modelo.getDireccion());
+    }
+    
+    public boolean Insertar(){
+        exito= false;
+        st= null;
+        cn= null;
+                
+        sql= "insert into cliente values ('"+
+                Modelo.getId()+"', '"+Modelo.getNombre()+"','"+Modelo.getDireccion()+"')";
+        
+        try{
+            cn= ConectarBD.Conectar();
+            st= cn.createStatement();
+            st.execute(sql);
+            
+            exito= true;
+            
+            st.close();
+            cn.close();
+        }catch(SQLException e){
+            System.out.println("Error al insert en BD");
+        }
+        return exito;
+    }
+}
