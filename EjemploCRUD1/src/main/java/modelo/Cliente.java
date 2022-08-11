357206786e25f41a7fd85777c906224421bf3d65
czunigac99 <czunigac@unah.hn>
2022-08-11 13:04:46 -0500

    Heredando atributos de persona a cliente

--- EjemploCRUD1/src/main/java/modelo/Cliente.java
+++ EjemploCRUD1/src/main/java/modelo/Cliente.java
@@ -5,6 +5,8 @@
  */
 package modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author cris
@@ -12,29 +14,29 @@ package modelo;
 public class Cliente extends Persona  {
     
 
-   String Direccion;
+   String CodigoCliente;
    
    public Cliente(){
     super();
    }
    
-     public Cliente (String Direccion){
+     public Cliente (String CodigoCliente){
        super();
-        this.Direccion=Direccion;
+        this.CodigoCliente=CodigoCliente;
     }
      
-     public Cliente(String Nombre, String Id,String Direccion) {
-	super(Nombre, Id);
-	this.Direccion = Direccion;
+     public Cliente(String Nombre, String Id,String CodigoCliente, String Telefono, Date Nacimiento, String Direccion, String Email) {
+	super(Nombre, Id, Telefono, Nacimiento, Direccion, Email);
+	this.CodigoCliente = CodigoCliente;
 	
      }
     
     
-    public String getDireccion(){
-    return Direccion;
+    public String getCodigoCliente(){
+    return CodigoCliente;
     }
-    public void setDireccion(String Direccion){
-    this.Direccion=Direccion;
+    public void setCodigoCliente(String CodigoCliente){
+    this.CodigoCliente=CodigoCliente;
     }
     
 }
